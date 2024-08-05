6cb4518c36729027319aa76beae044a9029102c5
Josh B <josh.bowman2021@gmail.com>
2024-08-04 17:11:57 -0700

    Added temporary docx-to-dita.js

---
+++ js/docx-to-dita.js
@@ -0,0 +1,6 @@
+function startDemo() {
+    const demoContainer = document.getElementById('demo-container');
+    demoContainer.innerHTML = `
+        <p>Online demo is not available yet. Please check back later.</p>
+    `;
+}
