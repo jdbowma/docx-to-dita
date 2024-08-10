b2da54e47353a1822782d76b712f837912319d64
Josh B <josh.bowman2021@gmail.com>
2024-08-09 19:24:45 -0700

    Reorganized

--- js/docx-to-dita.js
+++
@@ -1,6 +0,0 @@
-function startDemo() {
-    const demoContainer = document.getElementById('demo-container');
-    demoContainer.innerHTML = `
-        <p>Online demo is not available yet. Please check back later.</p>
-    `;
-}
